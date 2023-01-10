0093626a82ba00a76b0966cc383fc8041db1c34f
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2023-01-10 11:01:39 +0100

    fix sed commands

--- _compile.sh
+++ _compile.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-sed -i 's/\-Werror//g' $(find .)
-sed -i 's/\clang/afl-clang-lto -fsanitize=address,undefined/g' $(find .)
-sed -i 's/\cc/afl-clang-lto -fsanitize=address,undefined/g' $(find .)
-sed -i 's/\gcc/afl-clang-lto -fsanitize=address,undefined/g' $(find .)
+FILES=$(find . -type f)
+
+set -x \
+	&& sed -i 's/\-Werror//g' $FILES \
+	&& sed -i 's/\clang/afl-clang-lto -fsanitize=address,undefined/g' $FILES \
+	&& sed -i 's/\cc/afl-clang-lto -fsanitize=address,undefined/g' $FILES \
+	&& sed -i 's/\gcc/afl-clang-lto -fsanitize=address,undefined/g' $FILES
 export AFL_USE_ASAN=1
 export AFL_USE_UBSAN=1
 export AFL_USE_LSAN=1
